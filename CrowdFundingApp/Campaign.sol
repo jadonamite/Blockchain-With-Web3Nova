534874bd1a9a34f15fbedd6cd3b7601f21eccdc0
jadonamite <jadonamite@gmail.com>
2025-03-31 09:28:20 +0100

    More Updates

--- CrowdFundingApp/Campaign.sol
+++ CrowdFundingApp/Campaign.sol
@@ -2,3 +2,23 @@
 
 pragma solidity 0.8.26;
 
+import {Factory} from "CrowdFundingApp/Factory.sol";
+
+contract Campaign{
+
+    struct Contributor{
+        string name;
+        address contributorAddress;
+        uint contributions;
+      }
+      
+      address[] public contributors; // Addresses of the contributers
+      Contributor[] public allContributors;
+      mapping (address => uint) addressToContributions;
+      mapping(address => Contributor)  contributorsList;
+
+      
+    }
+
+
+
