52ec69166eb2266c1f14386dd546f336c6962896
jadonamite <jadonamite@gmail.com>
2025-03-31 15:38:01 +0100

    more

--- CrowdFundingApp/Campaign.sol
+++ CrowdFundingApp/Campaign.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.26;
 
 import {Factory} from "CrowdFundingApp/Factory.sol";
 
-contract Campaign{
+contract Campaign is Factory {
 
     struct Contributor{
         string name;
@@ -14,8 +14,34 @@ contract Campaign{
       
       address[] public contributors; // Addresses of the contributers
       Contributor[] public allContributors;
-      mapping (address => uint) addressToContributions;
+      mapping (uint => address) contributorId;
       mapping(address => Contributor)  contributorsList;
+      mapping(address => uint) addressToAmount;
+
+
+      // function contribute(uint contribution)public payable{
+      //   require(contribution >= 1 ether);
+      //   uint contributorCount = contributors.length;
+      //   require(contributorCount < 5,"Already 5 Contributers");
+      //   address contributerAddress = msg.sender;
+      //   require(!contributingInProcess[contributerAddress]);
+      //   contributingInProcess[contributerAddress] = true;
+      //   contributors.push(contributerAddress);
+      //   contributorId[contribution] = contributersList[contributerAddress].name;
+      //   allContributors.push(Contributor(contributersList[contributerAddress].name,msg.sender, contribution));
+      //   contributorsCount
+      // }
+
+      function getBalance(string memory title ) public view returns (uint){
+        return titleToCampaign[title].raisedAmount;
+      }
+
+       function getGoal(string memory title ) public view returns (uint){
+        return titleToCampaign[title].amountToRaise;
+      }
+      function getDeadline(string memory title ) public view returns (uint){
+        return titleToCampaign[title].deadline;
+      }
 
       
     }
